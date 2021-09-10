00b1044102a6fcffa32d5700eb9635216eef5185
April Day <april.day@gmail.com>
2021-09-10 18:54:43 +0100

    load hw file

--- gradebook.py
+++ gradebook.py
@@ -11,5 +11,13 @@ roster = pd.read_csv(
     index_col="NetID",
 )
 
+# load homework file
+hw_exam_grades=pd.read_csv(
+    DATA_FOLDER / "hw_exam_grades.csv",
+    converters={"SID": str.lower},
+    usecols=lambda x: "Submission" not in x,
+    index_col="SID",
+
+)
+
 #https://realpython.com/pandas-project-gradebook/
-# need to load homework file
